fb3a4a33dd36bb1f7e097493afcd76bfb02f587a
Neil Stoker <neil.stoker@gmail.com>
2012-12-06 13:07:13 +0000

    Added licence details and code::blocks template

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ minature-octo-test
 ==================
 
 Test code for segments of the minature-octo, the intention being to create a cli version of the simulator. Possibly.
+
+Licensed under GPLv3
+
+(C) 2012 Neil Stoker
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*     This file is part of Test65.
+
+    Test65 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Test65 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Test65.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Console Simulator for the lib65 project" << endl
+         << "Licenced under GPL v3, (c) 2012 Neil Stoker"<<endl;
+
+
+    cout <<"test65 Exiting."<< endl;
+    return 0;
+}
